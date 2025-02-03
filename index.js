3777db1b8f74de584da05335e34428df9721d021
Salami Azeez <salamitopeazeez@gmail.com>
2025-02-03 10:31:55 +0100

    Handled cors error

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// ============== MIDDLEWARES
+const allowedOrigins = [`http://localhost:${PORT}`];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) === -1) {
+            return callback(null, true);
+        }
+        const msg = 'Go baack!!!!! Origin not allowed by CORS';
+        return callback(new Error(msg), false);
+        
+    }
+}));
+
 
 
 // ============== START SERVER
